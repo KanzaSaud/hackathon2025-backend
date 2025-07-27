5202d846da8dc8bb10f425c0d0e0ef115664c09f
Kanza Saud <kanzasaud@gmail.com>
2025-07-27 16:28:47 +0500

    Fix vercel config and express export

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,25 @@
-import express from 'express';
+// import express from 'express';
 
-import {
-    create,
-    getAllFeedback,
-} from "../controller/userController.js"
+// import {
+//     create,
+//     getAllFeedback,
+// } from "../controller/userController.js"
 
-const route = express.Router();
+// const route = express.Router();
 
-route.post("/user", create);
-route.get("/users", getAllFeedback);
-// route.get("/user/:id", getUserById);
-// route.put("/update/user/:id", update);
-// route.delete("/delete/user/:id", deleteUser);
+// route.post("/user", create);
+// route.get("/users", getAllFeedback);
+// // route.get("/user/:id", getUserById);
+// // route.put("/update/user/:id", update);
+// // route.delete("/delete/user/:id", deleteUser);
 
-export default route;
+// export default route;
+
+const express = require("express");
+const router = express.Router();
+const { createUser, getAllUsers } = require("../controller/userController");
+
+router.post("/add", createUser);
+router.get("/users", getAllUsers);
+
+module.exports = router; // ✅ THIS IS CRITICAL
