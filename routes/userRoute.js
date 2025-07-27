a600558eb7fd087d40203fe7a690fe2f36c4153c
Kanza Saud <kanzasaud@gmail.com>
2025-07-27 17:06:13 +0500

    Pushing changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,25 +1,25 @@
-// import express from 'express';
+import express from 'express';
 
-// import {
-//     create,
-//     getAllFeedback,
-// } from "../controller/userController.js"
+import {
+    create,
+    getAllFeedback,
+} from "../controller/userController.js"
 
-// const route = express.Router();
+const route = express.Router();
 
-// route.post("/user", create);
-// route.get("/users", getAllFeedback);
-// // route.get("/user/:id", getUserById);
-// // route.put("/update/user/:id", update);
-// // route.delete("/delete/user/:id", deleteUser);
+route.post("/user", create);
+route.get("/users", getAllFeedback);
+// route.get("/user/:id", getUserById);
+// route.put("/update/user/:id", update);
+// route.delete("/delete/user/:id", deleteUser);
 
-// export default route;
+export default route;
 
-const express = require("express");
-const router = express.Router();
-const { createUser, getAllUsers } = require("../controller/userController");
+// const express = require("express");
+// const router = express.Router();
+// const { createUser, getAllUsers } = require("../controller/userController");
 
-router.post("/add", createUser);
-router.get("/users", getAllUsers);
+// router.post("/add", createUser);
+// router.get("/users", getAllUsers);
 
-module.exports = router; // ✅ THIS IS CRITICAL
+// module.exports = router; // ✅ THIS IS CRITICAL
